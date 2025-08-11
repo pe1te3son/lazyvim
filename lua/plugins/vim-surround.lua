f1f0400dc12a7838069a38ae6b2b66cc65873af7
Peter Janak <2010janak2010@gmail.com>
2025-08-11 11:52:57 +0100

    better yank

--- lua/plugins/vim-surround.lua
+++ lua/plugins/vim-surround.lua
@@ -2,9 +2,9 @@ return {
   "tpope/vim-surround",
   -- event = "VeryLazy",
   keys = {
-    { "cs", "<Plug>VSurround", mode = { "n", "x" }, desc = "Change Surrounding" },
-    { "ds", "<Plug>Dsurround", mode = { "n", "x" }, desc = "Delete Surrounding" },
-    { "ys", "<Plug>Ysurround", mode = { "n", "x" }, desc = "Add Surrounding" },
+    { "cs", "<Plug>VSurround", mode = { "n" }, desc = "Change Surrounding" },
+    { "ds", "<Plug>Dsurround", mode = { "n" }, desc = "Delete Surrounding" },
+    { "ys", "<Plug>Ysurround", mode = { "n" }, desc = "Add Surrounding" },
     { "yss", "<Plug>Yssurround", mode = { "n" }, desc = "Add Surrounding (Line)" },
     { "yS", "<Plug>YSsurround", mode = { "n" }, desc = "Add Surrounding (Block)" },
   },
