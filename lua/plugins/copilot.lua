a3959aa94156fb451c23bf98816c53296ecc002f
Peter Janak <2010janak2010@gmail.com>
2025-08-13 08:21:44 +0100

    added blink copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 0,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
       { "nvim-lua/plenary.nvim", branch = "master" },
