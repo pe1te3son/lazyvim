a3dd27c6db63556ff9649b7c676d07f084cab3a3
Peter Janak <2010janak2010@gmail.com>
2025-08-16 07:50:41 +0100

    update packages

--- lua/plugins/prev-manager.lua
+++ lua/plugins/prev-manager.lua
@@ -1,12 +1,3 @@
--- return {}
 return {
   "pe1te3son/prev-buffer.nvim",
-  keys = {
-    { "<leader>bd", desc = "Delete current buffer and switch to last" },
-    { "<leader><Tab>", desc = "Switch to last buffer" },
-  },
-  opts = {},
-  config = function(_, opts)
-    require("prev-manager").setup(opts)
-  end,
 }
