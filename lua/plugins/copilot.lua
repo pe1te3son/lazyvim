00eb450da736afd476e158c7211bacef368a249b
Peter Janak <2010janak2010@gmail.com>
2025-10-10 15:08:56 +0100

    enable copilot for some file types

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -16,44 +16,41 @@ return {
       -- suggestion = { enabled = false },
       -- panel = { enabled = false },
       filetypes = {
+        javascript = true,
+        typescript = true,
         markdown = true,
-        help = true,
+        python = true,
+        go = true,
+        lua = true,
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+            -- disable for .env files
+            return false
+          end
+          return true
+        end,
+        ["*"] = false,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 16.x
+      nes = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept_and_goto = "<leader>au",
+          accept = false,
+          dismiss = "<Esc>",
+        },
+      },
+      suggestion = {
+        enabled = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = "<C-j>",
+          accept_line = false,
+          next = "<C-l>",
+          prev = "<C-L>",
+        },
       },
     },
-    config = function()
-      require("copilot").setup({
-        filetypes = {
-          javascript = true,
-          typescript = true,
-          markdown = true,
-          terraform = false,
-          sh = function()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-              -- disable for .env files
-              return false
-            end
-            return true
-          end,
-        },
-        nes = {
-          enabled = true,
-          keymap = {
-            accept_and_goto = "<leader>au",
-            accept = false,
-            dismiss = "<Esc>",
-          },
-        },
-        suggestion = {
-          enabled = true,
-          keymap = {
-            accept = "<Tab>",
-            accept_word = "<C-j>",
-            accept_line = false,
-            next = "<C-l>",
-            prev = "<C-L>",
-          },
-        },
-      })
-    end,
   },
 }
